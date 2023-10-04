b48a9bdc58720c55ab464cf8462dfa9ffa100385
bargog29 <baranga.georgemarius@gmail.com>
2023-10-04 20:20:58 +0300

    Store and counter for products

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+'strict mode'
 const shop = document.getElementById('shop');
 
 // console.log(shop);
@@ -30,13 +31,43 @@ const shopData = [
     price: 10,
     description: 'Ullamco ea incididunt id fugiat',
     img: 'images/f4.jpg'
-}
+}, 
+{
+    id:'5th',
+    name: 'Nike T-Shirt V5',
+    price: 45,
+    description: 'Ullamco ea incididunt id fugiat',
+    img: 'images/f5.jpg'
+}, 
+{
+    id:'6th',
+    name: 'Nike T-Shirt V6',
+    price: 28,
+    description: 'Ullamco ea incididunt id fugiat',
+    img: 'images/f6.jpg'
+},
+{
+    id:'7th',
+    name: 'Nike Joggers V7',
+    price: 89,
+    description: 'Ullamco ea incididunt id fugiat',
+    img: 'images/f7.jpg'
+},
+{
+    id:'8th',
+    name: 'Nike Blouse V8',
+    price: 51,
+    description: 'Ullamco ea incididunt id fugiat',
+    img: 'images/f8.jpg'
+},
 ];
 
 let generateShop = () => {
     return (shop.innerHTML = shopData.map((data) => {
         // console.log(data);
         let {id, name, price, description, img} = data;
+        let search = counter.find((prod) => prod.id === id) || [];
+
         return`
             <div id=prod-id-${id} class="item">
             <img src="${img}" alt="">
@@ -46,9 +77,9 @@ let generateShop = () => {
                 <div class="price-quantity">
                     <h2>$${price}</h2>
                     <div class="buttons">
-                        <i class="bi bi-dash-lg"></i>
-                        <div id=${id} class="quantity">0</div>
-                        <i class="bi bi-plus-lg"></i>
+                        <i onclick="decrement('${id}')" class="bi bi-dash-lg"></i>
+                        <div id=${id} class="quantity">${search.item === undefined ? 0 : search.item}</div>
+                        <i onclick="increment('${id}')" class="bi bi-plus-lg"></i>
                         </div>
                     </div>
                 </div>
@@ -56,4 +87,60 @@ let generateShop = () => {
     }).join(''));
 };
 
+let counter = JSON.parse(localStorage.getItem('products')) || [];
+
 generateShop();
+
+let increment = (id) => {
+    let selectedItem = id;
+    
+    let search = counter.find((prod) => prod.id === selectedItem);
+
+    if(!search){
+        counter.push({
+        id: selectedItem,
+        item: 1,
+    })
+    }
+    else search.item += 1;
+
+    update(selectedItem);
+    // console.log(counter);    
+    localStorage.setItem("products", JSON.stringify(counter));
+
+};
+
+let decrement = (id) => {
+    let selectedItem = id;
+    
+    let search = counter.find((prod) => prod.id === selectedItem);
+
+    if(search === undefined) return;
+    else if(search.item === 0) return;
+    else search.item -= 1;
+
+    update(selectedItem);
+    counter = counter.filter((prod) => prod.item !== 0);
+
+    // console.log(counter);
+    localStorage.setItem("products", JSON.stringify(counter));
+};
+
+let update = (id) => {
+    let search = counter.find((prod) => prod.id === id);
+    // console.log(id);
+    try {
+        document.getElementById(id).innerHTML = search.item;
+    } catch (error) {
+        alert('Update failed. + :))'+'\n'+error);
+    }
+    newCounter();
+};
+
+let newCounter = () => {
+    let cartIcon = document.getElementById('cart-counter');
+    cartIcon.innerHTML = counter.map((prod) => prod.item)
+    .reduce((prev, next) => prev+next,0);
+};
+
+newCounter();
